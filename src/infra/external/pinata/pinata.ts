07726de664b8214c950fb6e276f7a3540e7ab6d9
pedro henrique <pedro.henrike2812@gmail.com>
2025-12-10 10:41:31 -0300

    debug

--- src/infra/external/pinata/pinata.ts
+++ src/infra/external/pinata/pinata.ts
@@ -2,7 +2,7 @@
 
 import { PinataSDK } from "pinata";
 
-console.log(process.env.PINATA_JWT);
+console.log(process.env);
 console.log(process.env.NEXT_PUBLIC_GATEWAY_URL);
 
 export const pinata = new PinataSDK({
